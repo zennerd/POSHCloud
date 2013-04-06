09d7a90208641528ab738b4f4d205ce046d9ac27
Derek Kruger <derek.kruger@rackspace.com>
2013-04-05 21:15:39 -0500

    create web servers

---
+++ CreateWebServers.ps1
@@ -0,0 +1,4 @@
+﻿$servers = Get-Content C:\scripts\servers.csv |
+ForEach-Object{
+Add-CloudServer -CloudServerName $_ -CloudServerFlavorID 2 -CloudServerImageID 8a3a9f96-b997-46fd-b7a8-a9e740796ffd -Region DFW
+}
